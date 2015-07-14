50d8b2f81eb001c0900043ea8d204138fbd22045
John Baumgartner <johnbaums@gmail.com>
2015-07-15 09:52:40 +1000

    Added type arg to ibra_region and adjacent_ibra - allows query by region or by subregion. Added sp dependency.

--- R/adjacent_ibra.R
+++ R/adjacent_ibra.R
@@ -4,25 +4,60 @@
 #' that contain, one or more given points.
 #'
 #' @param pts A SpatialPolygons* object.
-#' @return A SpatialPolygonsDataFrame representing the subset of polygons of
-#'   ibra7_albers within which one or more points (pts) fall, and those polygons
+#' @param min_n The minimum number of points a subregion must contain in order for 
+#'   it to be included in the returned subset.
+#' @param type Character. Either \code{'region'} or \code{'subregion'}. This 
+#'   determines how the IBRA polygons will be subset: \code{'region'} results in
+#'   subregion polygons bing returned even if they contain no points, as long as
+#'   other subregions of that region contain a total of at least \code{min_n}
+#'   points; \code{subregion} returns only the subregion polygons that contain
+#'   at least \code{min_n} points.
+#' @return A \code{SpatialPolygonsDataFrame} representing the subset of regions
+#'   (or subregions, see \code{type}) of \code{ibra7_albers} within which
+#'   \code{min_n} or more points (pts) fall, and those regions (or subregions)
 #'   adjacent to these.
 #' @keywords spatial, ibra
 #' @export
 #' @examples
 #' xy <- SpatialPoints(data.frame(x=runif(1000, 140, 145), y=runif(1000, -38, -35)))
 #' proj4string(xy) <- '+init=epsg:4283'
+#' # Sub-regions with at least 1 point
 #' adj <- adjacent_ibra(xy)
 #' plot(adj, col='gray80', border='transparent')
-adjacent_ibra <- function(pts, min_n) {
+#' # Sub-regions with at least 20 points
+#' adj2 <- adjacent_ibra(xy, min_n=20)
+#' plot(adj2, col='gray60', border='transparent', add=TRUE)
+#' # Regions with at least 1 point
+#' adj3 <- adjacent_ibra(xy, type='region')
+#' plot(adj3, col='gray80', border='transparent')
+#' # Regions with at least 20 points
+#' adj4 <- adjacent_ibra(xy, min_n=20, type='region')
+#' plot(adj4, col='gray60', border='transparent', add=TRUE)
+#' 
+#' # With plot_output=TRUE
+#' adj <- adjacent_ibra(xy, plot_output=TRUE)
+adjacent_ibra <- function(pts, min_n=1, type='subregion', plot_output=FALSE) {
+  if(!type %in% c('region', 'subregion'))
+    stop('type must be either "region" or "subregion".', call.=FALSE)
   if(is.na(proj4string(pts))) stop('pts has no CRS', call.=FALSE)
   if(proj4string(pts) != proj4string(ibra7_albers))
     pts_albers <- spTransform(pts, '+init=epsg:3577')
   o <- over(pts_albers, ibra7_albers)
-  tab <- table(o$REC_ID)
-  ibra7_albers[ibra7_albers$REC_ID %in% names(tab[tab >= min_n]), 'REC_ID']
-#   i <- row.names(ibra7_albers)[
-#     match(as.character(na.omit(unique(o$REG_NAME_7))), ibra7_albers$REG_NAME_7)]
-#   adj <- ibra7_albers[union(which(rowSums(ibra7_nbs[, i]) > 0),
-#                             match(i, rownames(ibra7_nbs))),]
+  out <- switch(type,
+         'region'={
+           tab <- table(o$REG_CODE_7)
+           ibra7_albers[ibra7_albers$REG_CODE_7 %in% 
+                          names(tab[tab >= min_n]), ]
+         },
+         'subregion'={
+           tab <- table(o$SUB_CODE_7)
+           ibra7_albers[ibra7_albers$SUB_CODE_7 %in% 
+                          names(tab[tab >= min_n]), ]
+           })
+  if(isTRUE(plot_output)) {
+    plot(out, col='gray80', border='gray70')
+    points(pts_albers, pch=20, 
+           col=ifelse(is.na(o$SUB_CODE_7), 'tomato2', 'gray10'))
+  }
+  out
 }
--- R/ibra_region.R
+++ R/ibra_region.R
@@ -1,34 +1,47 @@
-#' Identify IBRA 7 regions
+#' Identify IBRA 7 regions or subregions
 #'
-#' Return the names of the set of IBRA regions within which a set of points
-#' falls.
+#' Return the names of the set of IBRA regions or subregions within which a set
+#' of points falls.
 #'
 #' @param pts A SpatialPolygons* object.
 #' @param tabulate Logical. If \code{TRUE}, return a named vector indicating the
 #'   number of points in each occupied region.
 #' @param min_n The minimum number of points a region must contain in order for 
 #'   the region name to be returned when \code{tabulate} is \code{FALSE}.
-#' @return If \code{tabulate} is \code{FALSE}, a vector of IBRA 7 region names 
-#'   corresponding to the IBRA 7 regions that at least \code{min_n} \code{pts} 
-#'   fall within. If \code{tabulate} is \code{TRUE}, a named vector showing the 
-#'   number of points in each occupied IBRA 7 region. Points that do not overlie
-#'   an IBRA 7 region will be ignored with a warning.
+#' @param type Character. Either \code{'region'} or \code{'subregion'}. This 
+#'   determines how the IBRA polygons will be subset: \code{'region'} results in
+#'   subregion polygons bing returned even if they contain no points, as long as
+#'   other subregions of that region contain a total of at least \code{min_n}
+#'   points; \code{subregion} returns only the subregion polygons that contain
+#'   at least \code{min_n} points.
+#' @return If \code{tabulate} is \code{FALSE}, a vector of IBRA 7 subregion (or 
+#'   region, see \code{type}) names corresponding to the IBRA 7 regions (or 
+#'   subregions) that at least \code{min_n} \code{pts} fall within. If 
+#'   \code{tabulate} is \code{TRUE}, a named vector showing the number of points
+#'   in each occupied IBRA 7 subregions (or regions). Points that do not overlie
+#'   an IBRA 7 subregion will be ignored with a warning.
 #' @keywords spatial, ibra
 #' @export
 #' @examples
 #' xy <- SpatialPoints(data.frame(x=runif(1000, 140, 145), y=runif(1000, -38, -35)))
 #' proj4string(xy) <- '+init=epsg:4283'
-#' reg <- ibra_region(xy)
+#' reg <- ibra_region(xy, type='region')
 #' reg
-ibra_region <- function(pts, tabulate=TRUE, min_n=1) {
+#' subreg <- ibra_region(xy, type='subregion')
+#' subreg
+ibra_region <- function(pts, tabulate=TRUE, min_n=1, type='subregion') {
+  if(!type %in% c('region', 'subregion'))
+    stop('type must be either "region" or "subregion".', call.=FALSE)
   if(min_n != 1 & !tabulate) warning('If tabulate is TRUE, min_n is ignored.')
   if(is.na(proj4string(pts))) stop('pts has no CRS', call.=FALSE)
   if(proj4string(pts) != proj4string(ibra7_albers))
     pts_albers <- spTransform(pts, '+init=epsg:3577')
   o <- over(pts_albers, ibra7_albers)
-  if (any(is.na(o$REG_NAME_7)))
-    warning(sum(is.na(o$REG_NAME_7)), ' points did not coincide with IBRA 7 regions.')
-  tab <- table(as.character(o$REG_NAME_7))
+  if (any(is.na(o$SUB_CODE_7)))
+    warning(sum(is.na(o$SUB_CODE_7)), 
+            ' points did not coincide with IBRA 7 subregions.', call.=FALSE)
+  tab <- switch(type, 'region'=table(as.character(o$REG_NAME_7)),
+                'subregion'=table(as.character(o$SUB_NAME_7)))
   if(isTRUE(tabulate)) {
     return(c(tab))
   } else {
