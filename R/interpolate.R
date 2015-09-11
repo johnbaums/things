ec0329f3831ba0391063d9d5b60efa6798adc81b
John Baumgartner <johnbaums@gmail.com>
2015-09-11 15:12:45 +1000

    update objects to match arg names in interpolate

--- R/interpolate.R
+++ R/interpolate.R
@@ -42,61 +42,61 @@
 #' r2 <- raster(matrix(rnorm(100, mean=11), ncol=10))
 #' s <- stack(r1, r2)
 #' s_interp <- interpolateTemporal(s, c(2001, 2011), 2001:2020, tempdir(), 
-#'                                 'example', returnstack=TRUE)
+#'                                 'example', return_stack=TRUE)
 #' plot(s_interp, zlim=range(values(s_interp)))
 interpolate <- 
   function(s, z_in, z_out, outdir, prefix, progress=TRUE, write_change=TRUE, 
            return_stack=FALSE, ...) {
-    if(raster::nlayers(s) != length(xin)) 
-      stop('Length of xin must equal the number of layers in s.')
+    if(raster::nlayers(s) != length(z_in)) 
+      stop('Length of z_in must equal the number of layers in s.')
     if(raster::nlayers(s) < 2) 
       stop('stack s must have at least 2 layers.')
-    if(!all(findInterval(xout, range(xin), rightmost.closed=TRUE) == 1)) {
-      if(any(xout < min(xin))) {
+    if(!all(findInterval(z_out, range(z_in), rightmost.closed=TRUE) == 1)) {
+      if(any(z_out < min(z_in))) {
         stop('This function does not extrapolate backwards (i.e. below the ',
-             'earliest element in xin). All elements of xout must be greater ',
-             'than min(xin).')
+             'earliest element in z_in). All elements of z_out must be greater ',
+             'than min(z_in).')
       } else {      
-        warning('Some values of xout require extrapolation beyond the range of ',
-                'xin.\nThe rate of change for extrapolation is assumed to be ',
+        warning('Some values of z_out require extrapolation beyond the range of ',
+                'z_in.\nThe rate of change for extrapolation is assumed to be ',
                 'equal to that for the period between the last and ',
-                'second-to-last elements of xin (after temporal ordering).')
+                'second-to-last elements of z_in (after temporal ordering).')
       }
     }
     outdir <- normalizePath(sub('/$|\\\\$', '', outdir), winslash='/', 
                             mustWork=FALSE)
     if(!file.exists(outdir)) dir.create(outdir, recursive=TRUE)
-    xout <- unique(xout)
-    if(is.unsorted(xin)) {
-      s <- s[[order(xin)]]
-      xin <- sort(xin)
+    z_out <- unique(z_out)
+    if(is.unsorted(z_in)) {
+      s <- s[[order(z_in)]]
+      z_in <- sort(z_in)
     }
-    len <- diff(xin)
-    base <- findInterval(xout, xin)
+    len <- diff(z_in)
+    base <- findInterval(z_out, z_in)
     lower <- unique(base[base < nlayers(s)])
     s.change <- raster::stack(
       sapply(if(length(lower) > 0) lower else raster::nlayers(s) - 1, 
              function(x) {
                message(
                  sprintf('Calculating change grid for %s to %s.', 
-                         xin[x], xin[x+1])
+                         z_in[x], z_in[x+1])
                )
                raster::overlay(
                  s[[x]], s[[x+1]], fun=function(x1, x2) (x2-x1)/len[x],
                  filename=ifelse(
-                   writechange, 
+                   write_change, 
                    file.path(outdir, sprintf('changegrid_%s_%s', 
-                                             xin[x], xin[x+1])), 
+                                             z_in[x], z_in[x+1])), 
                    ''), recycle=FALSE, format='GTiff', ...)
              })
     )
-    multi <- xout - xin[base]
+    multi <- z_out - z_in[base]
     chg.ind <- ifelse(base > raster::nlayers(s.change), 
                       raster::nlayers(s.change), base)
-    message('Calculating grids for years specified in xout...')
-    if(progress) pb <- txtProgressBar(0, length(xout), style=3)
-    invisible(sapply(seq_along(xout), function(x) {
-      out.rast <- if(xout[x] %in% xin) {
+    message('Calculating grids for years specified in z_out...')
+    if(progress) pb <- txtProgressBar(0, length(z_out), style=3)
+    invisible(sapply(seq_along(z_out), function(x) {
+      out.rast <- if(z_out[x] %in% z_in) {
         s[[base[x]]]
       } else {
         raster::overlay(s[[base[x]]], s.change[[chg.ind[x]]],
@@ -104,12 +104,12 @@ interpolate <-
       }
       raster::writeRaster(
         out.rast, 
-        filename=file.path(outdir, sprintf('%s_%s', prefix, xout[x])), 
+        filename=file.path(outdir, sprintf('%s_%s', prefix, z_out[x])), 
         format='GTiff', ...)
       if(progress) setTxtProgressBar(pb, x)
     }))
-    if(isTRUE(returnstack)) {
-      f <- file.path(outdir, paste0(prefix, '_', xout, '.tif'))
+    if(isTRUE(return_stack)) {
+      f <- file.path(outdir, paste0(prefix, '_', z_out, '.tif'))
       return(raster::stack(
         f[order(as.numeric(sub('.*_(\\d+)\\.tif$', '\\1', f)))]
       ))
